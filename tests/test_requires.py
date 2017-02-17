d585a5bac91199cfcbc48c04800f2c6531fac768
Shezad Khan <shezadkhan137@gmail.com>
2017-02-17 13:05:47 +0000

    Add expression tests

--- tests/test_requires.py
+++ tests/test_requires.py
@@ -56,3 +56,41 @@ class TestRequires(object):
         data = {"x": 1, "y": 2}
         requires.validate("y", data)
         requires.validate("x", data)
+
+    def test_simple_expression_dependency_equal(self):
+        requires = Requires("x", R("y") == 1)
+        data = {"x": 1, "y": 2}
+        with pytest.raises(RequirementError):
+            requires.validate("x", data)
+
+        data = {"x": 1, "y": 1}
+        requires.validate("x", data)
+
+    def test_simple_expression_dependency_greater_or_equal(self):
+        requires = Requires("x", R("y") >= 1)
+        data = {"x": 1, "y": 0}
+        with pytest.raises(RequirementError):
+            requires.validate("x", data)
+
+        data = {"x": 1, "y": 1}
+        requires.validate("x", data)
+
+        data = {"x": 1, "y": 2}
+        requires.validate("x", data)
+
+        data = {"y": 2}
+        requires.validate("y", data)
+
+    def test_multi_expression_dependency_correctly_validates(self):
+        requires = Requires("x", R("y") >= 1) + Requires("z", R("y") <= 1)
+
+        data = {"x": 1, "y": 2, "z": 1}
+
+        with pytest.raises(RequirementError):
+            requires.validate("z", data)
+
+        requires.validate("x", data)
+        requires.validate("y", data)
+
+        data = {"x": 1, "y": 1, "z": 1}
+        requires.validate("z", data)
