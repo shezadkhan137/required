0533ed6b2e5754e4fbf45ad55412946a7d7e3cd0
Shezad Khan <shaz@revl.world>
2017-03-17 12:25:52 +0000

    Bump to 0.2.2

--- src/required/__init__.py
+++ src/required/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 from .requires import Requires, RequirementError, validate
 from .expressions import R
 
-__version__ = "0.1.4"
+__version__ = "0.2.2"
 
 __title__ = "required"
 __description__ = "A easy dependency validator"
